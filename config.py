1651d80a890f73ebe07d6196a347935656ae245b
Krati Jain <120194495+Tipsy-12@users.noreply.github.com>
2025-06-18 23:23:24 +0000

    Fix dotenv in config and add .gitignore for .env and cache

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GOOGLE_API_KEY = os.getenv("AIzaSyDcyPkDBHE2HaNe9Hn_HZgu-RhBywWLSl0")
-LANGCHAIN_API_KEY = os.getenv("AIzaSyDcyPkDBHE2HaNe9Hn_HZgu-RhBywWLSl0", "")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY", "")
 LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2", "false")
+
 PDF_PATH = "NIPS-2017-attention-is-all-you-need-Paper.pdf"
