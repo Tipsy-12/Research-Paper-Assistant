5106eec87b5043fa037c7097d75968485c0dc9ff
Krati Jain <120194495+Tipsy-12@users.noreply.github.com>
2025-06-18 22:56:22 +0000

    changes in config

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 GOOGLE_API_KEY = os.getenv("AIzaSyDcyPkDBHE2HaNe9Hn_HZgu-RhBywWLSl0")
 LANGCHAIN_API_KEY = os.getenv("AIzaSyDcyPkDBHE2HaNe9Hn_HZgu-RhBywWLSl0", "")
 LANGCHAIN_TRACING_V2 = os.getenv("LANGCHAIN_TRACING_V2", "false")
-PDF_PATH = "data/your_paper.pdf"
+PDF_PATH = "NIPS-2017-attention-is-all-you-need-Paper.pdf"
